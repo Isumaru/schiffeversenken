438153dd061fad4de299626895557939ed381fc3
alex0070071 <alex0070071@gmail.com>
2012-12-19 21:07:14 +0100

    Ausgabe des Higscores

---
+++ Schiffeversenken/Schiffeversenken/score.c
@@ -0,0 +1,5 @@
+
+
+
+void addScore(char name[20], int wtreffer, int strever,int gewonnen,int verloren){
+}
---
+++ Schiffeversenken/Schiffeversenken/score.h
@@ -0,0 +1,11 @@
+#ifndef _SCORE_H_
+#define _SCORE_H_
+void addScore(
+
+
+
+
+
+
+
+#endif
