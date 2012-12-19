92e157d5181dfe7c899a8994f52fac3fdf2951f1
alex0070071 <alex0070071@gmail.com>
2012-12-19 21:18:29 +0100

    Ausgabe des Highscores(Du musst das noch anpassen)

--- Schiffeversenken/Schiffeversenken/score.c
+++ Schiffeversenken/Schiffeversenken/score.c
@@ -1,5 +1,24 @@
-
-
+#include <stdio.h>
+#include <conio.h>
 
 void addScore(char name[20], int wtreffer, int strever,int gewonnen,int verloren){
+	double verhelt=0.0;
+
+	FILE*score;
+	score=fopen("hallo.txt", "a");
+	verhelt=(double)(wtreffer+strever)/100*strever;
+	fprintf(score, "%s %i %i %.2lf \n",name,gewonnen,verloren,verhelt);
+	fclose(score);
+
+
+}
+
+
+void showScore(){
+	char ausgabe[80];
+	FILE*score;
+	score=fopen("hallo.txt","r");
+	while(fgets(ausgabe,80,score)!= NULL){
+		printf("%s\n",ausgabe);
+	}
 }
